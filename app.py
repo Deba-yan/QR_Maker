4707d7f562cc2d2e836945a620ef01031406ca64
Debayan kundu <debayankundu02@gmail.com>
2026-08-23 16:10:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, send_file
+import qrcode
+import io
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return open("index.html", encoding="utf-8").read()
+
+@app.route("/style.css")
+def style():
+    return open("style.css", encoding="utf-8").read(), 200, {
+        "Content-Type": "text/css"
+    }
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    text = request.form["link"]
+
+    qr = qrcode.make(text)
+
+    buffer = io.BytesIO()
+    qr.save(buffer, "PNG")
+    buffer.seek(0)
+
+    return send_file(buffer, mimetype="image/png")
+
+app.run(debug=True)
